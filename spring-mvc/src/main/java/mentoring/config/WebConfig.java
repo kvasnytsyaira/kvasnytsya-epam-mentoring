adac19cf1a7995eb810543f0ace6604f8775fe79
Iryna_Kvasnytsya <ikvasnytsya@vertafore.com>
2022-08-11 16:36:59 +0300

    hot fix

--- spring-mvc/src/main/java/mentoring/config/WebConfig.java
+++ spring-mvc/src/main/java/mentoring/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/");
+        resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         return resolver;
     }
