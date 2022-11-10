12bb36ed5c458409f5f252ec59bd07d1a9fe7a79
Iryna_Kvasnytsya <iryna_kvasnytsya@epam.com>
2022-11-10 21:06:46 +0200

    rest

--- rest/src/main/java/Application.java
+++ rest/src/main/java/Application.java
@@ -3,9 +3,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@SpringBootApplication(scanBasePackages = "kvasnytsya-epam-mentoring/rest")
-//@ComponentScan({"event-service-api", "event-service-dto","event-service-impl","event-service-rest"})
-@ComponentScan(basePackages = "com.org")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
